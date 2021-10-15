c91c63ce58f370ffb498c20d69b49d8f97ea7b3f
garywilki <wil20223@byui.edu>
2021-10-14 17:01:33 -0700

    Gary's modification

--- src/ndlovu/Main.java
+++ src/ndlovu/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	    String name = "Nigel";
+        String name2 = "Gary";
 	    System.out.format("Hello, my name is %s.", name);
+        System.out.format("\nHello %s, my name is %s.", name, name2);
     }
 }
